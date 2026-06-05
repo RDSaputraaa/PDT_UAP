f4fec10acf4b0bcf82267909541a27520381eb60
lia <yuliailuy6@gmail.com>
2026-06-05 15:53:42 +0700

    Update login.php

--- public/login.php
+++ public/login.php
@@ -1,1 +1,26 @@
+<?php
+require_once '../config/database.php';
+require_once '../app/controllers/AuthController.php';
 
+$auth = new AuthController($conn);
+$error = '';
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    if($auth->login($_POST['username'], $_POST['password'])){
+        header('Location: index.php');
+        exit;
+    }
+    $error='Username atau password salah';
+}
+?>
+<!DOCTYPE html>
+<html><body>
+<h2>Login</h2>
+<?php if($error) echo "<p>$error</p>"; ?>
+<form method="post">
+<input name="username" placeholder="Username" required><br><br>
+<input type="password" name="password" placeholder="Password" required><br><br>
+<button type="submit">Login</button>
+</form>
+<a href="register.php">Register</a>
+</body></html>
