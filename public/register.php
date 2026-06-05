666a7a111893060d88e101b031263776347fdf2f
lia <yuliailuy6@gmail.com>
2026-06-05 15:58:06 +0700

    Implement user registration functionality

--- public/register.php
+++ public/register.php
@@ -1,1 +1,24 @@
+<?php
+require_once '../config/database.php';
+require_once '../app/controllers/AuthController.php';
 
+$auth = new AuthController($conn);
+$msg='';
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $msg = $auth->register($_POST['username'], $_POST['password'])
+        ? 'Registrasi berhasil'
+        : 'Registrasi gagal';
+}
+?>
+<!DOCTYPE html>
+<html><body>
+<h2>Register</h2>
+<p><?php echo $msg; ?></p>
+<form method="post">
+<input name="username" required placeholder="Username"><br><br>
+<input type="password" name="password" required placeholder="Password"><br><br>
+<button type="submit">Register</button>
+</form>
+<a href="login.php">Login</a>
+</body></html>
