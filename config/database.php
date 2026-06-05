814017ef442ecaa781bbee7c2f4ecebb4f528794
KarinaAini <karinaaini354@gmail.com>
2026-06-05 19:28:07 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'amanparkir');
+define('DB_NAME', 'aman_parkir');
 define('DB_PORT', 3306);
 
 try {
